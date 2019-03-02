b3687fded5267d6f61e7e87bb242fe7ed08f7aef
Muralidhar Basani <murali.basani@gmail.com>
2019-03-02 10:23:18 +0100

    Package restructuring

--- src/main/java/com/kafkamgt/clusterapi/config/SwaggerConfig.java
+++ src/main/java/com/kafkamgt/clusterapi/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.kafkamgt.clusterapi;
+package com.kafkamgt.clusterapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
